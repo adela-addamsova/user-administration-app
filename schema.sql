1ad65653e59518f0c0591a7f442c94ce0be0e56b
adela-addamsova <a.addamsova@gmail.com>
2024-12-10 18:27:18 +0100

    fix: adjust users table to have a new column deleted_at with default value NULL

--- schema.sql
+++ schema.sql
@@ -8,7 +8,9 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `deleted_at` timestamp NOT NULL DEFAULT NULL 
+  ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
 
