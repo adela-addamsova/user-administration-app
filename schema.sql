08aca6e45c036a1e655d4876f53f95882496de45
adela-addamsova <a.addamsova@gmail.com>
2024-12-12 18:04:00 +0100

    fix: correctly add null value for users table fix: add use DATABASE for schema feat: add UNIQUE for email and login column to use it in presenters

--- schema.sql
+++ schema.sql
@@ -1,18 +1,19 @@
 CREATE DATABASE UserApp;
+use DATABASE UserApp;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `login` varchar(100) NOT NULL,
+  `login` varchar(100) NOT NULL UNIQUE,
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `deleted_at` timestamp NOT NULL DEFAULT NULL 
+  `deleted_at` timestamp DEFAULT NULL 
   ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `login_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -22,7 +23,7 @@ CREATE TABLE `login_logs` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `login_logs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 
