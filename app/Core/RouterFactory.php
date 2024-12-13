9a4526384c1f5959803b60d2a2ab97dee499fc40
adela-addamsova <a.addamsova@gmail.com>
2024-12-13 12:34:39 +0100

    style: delete not neccessary routes

--- app/Core/RouterFactory.php
+++ app/Core/RouterFactory.php
@@ -12,13 +12,9 @@ class RouterFactory
     public static function createRouter(): RouteList
     {
         $router = new RouteList;
-        $router[] = new Route('/home', 'Home:default');
-		$router[] = new Route('/', 'Login:login');
-        $router[] = new Route('/sign-up', 'SignUp:signup');
-        $router[] = new Route('/dashboard', 'Dashboard:dashboard');
-		$router[] = new Route('/dashboard/logout', 'Dashboard:logout');
-        $router[] = new Route('/dashboard/createUser', 'Signup:create');
-        $router[] = new Route('/dashboard/editUser/<id>', 'EditUser:editUser');
+		
+        $router[] = new Route('/<presenter>/<action>');
+        $router[] = new Route('/', 'Login:login');
         return $router;
     }
 }
